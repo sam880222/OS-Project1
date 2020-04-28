8ccfc9b2417f3eadd64180a6f51b4adcedf2b09b
sam880222 <sam880222@gmail.com>
2020-04-28 18:07:50 +0800

    kernel_code

--- kernel_files/my_gettime.c
+++ kernel_files/my_gettime.c
@@ -2,10 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/timer.h>
 
-asmlinkage char* sys_my_gettime(void) {
-    struct timespec t;
-    getnstimeofday(&t);
-    char ret_time[30];
-    sprintf(ret_time, "%ld.%09ld", t.tv_sec, t.tv_nsec);
-    return ret_time;
+asmlinkage void sys_my_gettime(struct timespec *t) {    
+    getnstimeofday(t);    
+    return ;
 }
